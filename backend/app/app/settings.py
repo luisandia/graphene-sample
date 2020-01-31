e894a44996bb3c6fcbe5a6400905b82e18276d0c
luisandia <luisandiamamani@gmail.com>
2020-01-30 23:38:15 -0500

    CRUD complete

--- backend/app/app/settings.py
+++ backend/app/app/settings.py
@@ -43,8 +43,11 @@ INSTALLED_APPS = [
     'tracks'
 ]
 
-GRAPHENE={
-    'SCHEMA':'app.schema.schema'
+GRAPHENE = {
+    'SCHEMA': 'app.schema.schema',
+    'MIDDLEWARE': [
+        'graphql_jwt.middleware.JSONWebTokenMiddleware',
+    ],
 }
 
 MIDDLEWARE = [
@@ -55,6 +58,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+
+]
+AUTHENTICATION_BACKENDS = [
+    'graphql_jwt.backends.JSONWebTokenBackend',
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 ROOT_URLCONF = 'app.urls'
