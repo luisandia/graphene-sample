5f2d5de2a6bda95119c8df66c3e36b30b51a1284
luisandia <luisandiamamani@gmail.com>
2020-02-02 02:51:19 -0500

    Add TrackList Component

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,14 +1,53 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
+import SearchStracks from '../components/Track/SearchTracks';
+import CreateTrack from "../components/Track/CreateTrack";
+import TrackList from "../components/Track/TrackList";
+import { Query } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import Loading from "../components/Shared/Loading";
+import Error from "../components/Shared/Error";
 
 const App = ({ classes }) => {
-  return <div>App</div>;
+  return <div className={classes.container}>
+    <SearchStracks />
+    <CreateTrack />
+    <Query query={GET_TRACKS_QUERY}>
+      {
+        ({ data, loading, error }) => {
+          if (loading) return <Loading />
+          if (error) return <Error error={error} />
+          return <TrackList tracks={data.tracks} />
+        }
+      }
+    </Query>
+  </div>;
 };
+
+
+const GET_TRACKS_QUERY = gql`
+
+query getTracksQuery {
+  tracks {
+    id
+    title
+    description
+    url
+    likes{
+      id
+    }
+    postedBy{
+      id
+      username
+    }
+  }
+}
+`;
 
 const styles = theme => ({
   container: {
     margin: "0 auto",
-    maxWidth: 960,
+    maxWidth: 1200,
     padding: theme.spacing(1) * 2
   }
 });
